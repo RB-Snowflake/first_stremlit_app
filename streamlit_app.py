11c66aa8b646f6e5ce5e8e52bc501770526e783b
RB-Snowflake <149153336+RB-Snowflake@users.noreply.github.com>
2023-10-27 12:24:13 +0200

    Aggiunta la fonte dati csv

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,7 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal');
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie');
 streamlit.text('🐔 Hard-boiled Free-Range Egg');
 streamlit.text('🥑🍞 Avocado Toast');
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇');
+
+my_fruit_list = pandas.read.csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
+stermlit.dataframe(my_fruit_list);
