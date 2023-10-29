bac054dbe9591c9629f7f357d55be4dbbcc7e16d
RB-Snowflake <149153336+RB-Snowflake@users.noreply.github.com>
2023-10-29 09:29:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# spacchettamento del json 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# visualizzazione del json a video
+streamlit.dataframe(fruityvice_normalized)
