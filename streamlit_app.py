dbdd015ad1895f4e4fadd55400f46e764ebbbed2
RB-Snowflake <149153336+RB-Snowflake@users.noreply.github.com>
2023-10-29 11:44:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ streamlit.dataframe(fruityvice_normalized)
 #Informazioni di connessione a snoflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from PC_RIVERY_DB.PUBLIC.fruit_load_lists")
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.text("HThe fruit load list contains:")
 streamlit.text(my_data_row)
