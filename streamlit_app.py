f0cb327dd52c48ff2f151dbb86644c09b0224bc5
RB-Snowflake <149153336+RB-Snowflake@users.noreply.github.com>
2023-10-29 11:50:55 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,7 @@ my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+# Let's put a pick list from the table
+add_my_fruit = streamlit.multiselect("Pick some fruits:", list(my_data_rows.index),['Avocado','Strawberries'])
+fruits_to_show = my_data_rows.loc[add_my_fruit]
